eef156677d150bdd6705750e19bc786a7d832b48
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-05 16:32:16 +0300

    ijunior_csharp_dz42

---
+++ vn_mode_csharp_dz42/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class MyClass
+{
+    private string myProperty;
+
+    public MyClass(string myProperty)
+    {
+        this.myProperty = myProperty;
+    }
+
+    public void MyMethod()
+    {
+        Console.WriteLine("Мой метод выполняется.");
+    }
+
+    public string MyProperty
+    {
+        get { return myProperty; }
+        set { myProperty = value; }
+    }
+
+    public static void Main()
+    {
+        MyClass myObject = new MyClass("Пример");
+        myObject.MyMethod();
+        myObject.MyProperty = "Новое значение";
+        Console.WriteLine("Значение свойства: " + myObject.MyProperty);
+    }
+}
